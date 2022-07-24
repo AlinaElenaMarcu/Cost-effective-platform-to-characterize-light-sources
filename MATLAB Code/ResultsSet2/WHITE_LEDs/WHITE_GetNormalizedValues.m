926bf875a1a5755acf826fdbf484266e976cea89
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 19:38:34 +0300

    update WHITE_GetNormalizedValues

---
+++ MATLAB Code/ResultsSet2/WHITE_LEDs/WHITE_GetNormalizedValues.m
@@ -0,0 +1,18 @@
+% Normalize spectrum values
+
+% WHITE_AS7263
+spectrum_W1_TL_norm = normalize(spectrum_W1_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_W1_AS_norm = normalize(spectrum_W1_AS,'norm','inf'); % AS7262 values normalized
+
+% WHITE_LUPA
+spectrum_W2_TL_norm = normalize(spectrum_W2_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_W2_AS_norm = normalize(spectrum_W2_AS,'norm','inf'); % AS7262 values normalized
+
+% WHITE_NEXUS5X_FLASH
+spectrum_W3_TL_norm = normalize(spectrum_W3_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_W3_AS_norm = normalize(spectrum_W3_AS,'norm','inf'); % AS7262 values normalized
+
+% WHITE_STRIP
+WHITE_STRIP_TL_norm = normalize(WHITE_STRIP_TL,'norm','inf'); % ThorLabs values normalized
+WHITE_STRIP_AS_norm = normalize(WHITE_STRIP_AS,'norm','inf'); % AS7262 values normalized
+
