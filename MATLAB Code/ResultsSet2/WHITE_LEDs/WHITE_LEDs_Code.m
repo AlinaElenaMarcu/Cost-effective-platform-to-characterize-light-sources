900c6fcea41538aa70466b6f6c15cfabba609c3f
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 19:34:47 +0300

    update WHITE_LEDs_Code and GetVariablesFromFiles

--- MATLAB Code/ResultsSet2/WHITE_LEDs/WHITE_LEDs_Code.m
+++ MATLAB Code/ResultsSet2/WHITE_LEDs/WHITE_LEDs_Code.m
@@ -1,2 +1,5 @@
-% Plot all the LEDs in the WHITE category -- WHITE_AS7263, ORANGE2, ORANGE3, 
-% ORANGE4, ORANGE5, ORANGE6
+% Plot all the LEDs in the WHITE category -- WHITE_AS7263, WHITE_LUPA, WHITE_NEXUS5X_FLASH, WHITE_STRIP
+
+% Load WHITE LEDs variables -- ThorLabs and AS7262
+
+WHITE_GetVariablesFromFiles;
