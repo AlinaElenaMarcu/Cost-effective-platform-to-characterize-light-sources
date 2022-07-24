fef05d90619082a47bb1536e525627f797c2cf23
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 13:09:29 +0300

    update GREEN_GetNormalizedValues.m

--- MATLAB Code/ResultsSet2/GREEN_LEDs/GREEN_GetNormalizedValues.m
+++ MATLAB Code/ResultsSet2/GREEN_LEDs/GREEN_GetNormalizedValues.m
@@ -1,1 +1,33 @@
 % Normalize spectrum values
+
+% GREEN1
+spectrum_G1_TL_norm = normalize(spectrum_G1_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_G1_AS_norm = normalize(spectrum_G1_AS,'norm','inf'); % AS7262 values normalized
+
+% GREEN2
+spectrum_G2_TL_norm = normalize(spectrum_G2_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_G2_AS_norm = normalize(spectrum_G2_AS,'norm','inf'); % AS7262 values normalized
+
+% GREEN3
+spectrum_G3_TL_norm = normalize(spectrum_G3_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_G3_AS_norm = normalize(spectrum_G3_AS,'norm','inf'); % AS7262 values normalized
+
+% GREEN4
+spectrum_G4_TL_norm = normalize(spectrum_G4_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_G4_AS_norm = normalize(spectrum_G4_AS,'norm','inf'); % AS7262 values normalized
+
+% GREEN5_KING_SGC
+spectrum_G5_TL_norm = normalize(spectrum_G5_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_G5_AS_norm = normalize(spectrum_G5_AS,'norm','inf'); % AS7262 values normalized
+
+% GREEN_RG_2THT
+spectrum_GRG2T_TL_norm = normalize(spectrum_GRG2T_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_GRG2T_AS_norm = normalize(spectrum_GRG2T_AS,'norm','inf'); % AS7262 values normalized
+
+% GREEN_RG_3THT
+spectrum_GRG3T_TL_norm = normalize(spectrum_GRG3T_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_GRG3T_AS_norm = normalize(spectrum_GRG3T_AS,'norm','inf'); % AS7262 values normalized
+
+% GREEN_STRIP
+spectrum_GSTRIP_TL_norm = normalize(spectrum_GSTRIP_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_GSTRIP_AS_norm = normalize(spectrum_GSTRIP_AS,'norm','inf'); % AS7262 values normalized
