cb1385c27f1db31855394185cecfcba121daf0b5
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 13:42:50 +0300

    update GREEN_GetDatafromFittedPlots

---
+++ MATLAB Code/ResultsSet2/GREEN_LEDs/GREEN_GetDatafromFittedPlots.m
@@ -0,0 +1,57 @@
+% Get xData and yData from fitted plots
+
+% GREEN1
+spectrum_G1_TL_xData = get(plot(spectrum_G1_TL_fit),'xData')';
+spectrum_G1_TL_yData = get(plot(spectrum_G1_TL_fit),'yData')';
+
+spectrum_G1_AS_xData = get(plot(spectrum_G1_AS_fit),'xData')';
+spectrum_G1_AS_yData = get(plot(spectrum_G1_AS_fit),'yData')';
+
+% GREEN2
+spectrum_G2_TL_xData = get(plot(spectrum_G2_TL_fit),'xData')';
+spectrum_G2_TL_yData = get(plot(spectrum_G2_TL_fit),'yData')';
+
+spectrum_G2_AS_xData = get(plot(spectrum_G2_AS_fit),'xData')';
+spectrum_G2_AS_yData = get(plot(spectrum_G2_AS_fit),'yData')';
+
+% GREEN3
+spectrum_G3_TL_xData = get(plot(spectrum_G3_TL_fit),'xData')';
+spectrum_G3_TL_yData = get(plot(spectrum_G3_TL_fit),'yData')';
+
+spectrum_G3_AS_xData = get(plot(spectrum_G3_AS_fit),'xData')';
+spectrum_G3_AS_yData = get(plot(spectrum_G3_AS_fit),'yData')';
+
+% GREEN4
+spectrum_G4_TL_xData = get(plot(spectrum_G4_TL_fit),'xData')';
+spectrum_G4_TL_yData = get(plot(spectrum_G4_TL_fit),'yData')';
+
+spectrum_G4_AS_xData = get(plot(spectrum_G4_AS_fit),'xData')';
+spectrum_G4_AS_yData = get(plot(spectrum_G4_AS_fit),'yData')';
+
+% GREEN5_KING_SGC
+spectrum_G5_TL_xData = get(plot(spectrum_G5_TL_fit),'xData')';
+spectrum_G5_TL_yData = get(plot(spectrum_G5_TL_fit),'yData')';
+
+spectrum_G5_AS_xData = get(plot(spectrum_G5_AS_fit),'xData')';
+spectrum_G5_AS_yData = get(plot(spectrum_G5_AS_fit),'yData')';
+
+% GREEN_RG_2THT
+spectrum_GRG2T_TL_xData = get(plot(spectrum_GRG2T_TL_fit),'xData')';
+spectrum_GRG2T_TL_yData = get(plot(spectrum_GRG2T_TL_fit),'yData')';
+
+spectrum_GRG2T_AS_xData = get(plot(spectrum_GRG2T_AS_fit),'xData')';
+spectrum_GRG2T_AS_yData = get(plot(spectrum_GRG2T_AS_fit),'yData')';
+
+% GREEN_RG_3THT
+spectrum_GRG3T_TL_xData = get(plot(spectrum_GRG3T_TL_fit),'xData')';
+spectrum_GRG3T_TL_yData = get(plot(spectrum_GRG3T_TL_fit),'yData')';
+
+spectrum_GRG3T_AS_xData = get(plot(spectrum_GRG3T_AS_fit),'xData')';
+spectrum_GRG3T_AS_yData = get(plot(spectrum_GRG3T_AS_fit),'yData')';
+
+% GREEN_STRIP
+spectrum_GSTRIP_TL_xData = get(plot(spectrum_GSTRIP_TL_fit),'xData')';
+spectrum_GSTRIP_TL_yData = get(plot(spectrum_GSTRIP_TL_fit),'yData')';
+
+spectrum_GSTRIP_AS_xData = get(plot(spectrum_GSTRIP_AS_fit),'xData')';
+spectrum_GSTRIP_AS_yData = get(plot(spectrum_GSTRIP_AS_fit),'yData')';
