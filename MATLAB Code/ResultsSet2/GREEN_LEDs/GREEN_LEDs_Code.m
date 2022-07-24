fb50d65f78384c9c69bbfb6a28aac94ce807843c
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 12:08:11 +0300

    create GREEN_LEDs_Code.m

---
+++ MATLAB Code/ResultsSet2/GREEN_LEDs/GREEN_LEDs_Code.m
@@ -0,0 +1,4 @@
+% Plot all the LEDs in the GREEN category -- GREEN1, GREEN2, GREEN3, GREEN4, 
+% GREEN5_KING_SGC, GREEN_RG_2THT, GREEN_RG_3THT, GREEN_STRIP
+
+% Load GREEN LEDs variables -- ThorLabs and AS7262
