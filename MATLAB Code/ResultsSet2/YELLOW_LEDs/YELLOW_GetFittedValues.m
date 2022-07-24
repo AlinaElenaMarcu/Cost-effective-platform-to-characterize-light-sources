4d095839b52a0962f246b2b2ae49c5950afac50b
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 23:14:48 +0300

    Update YELLOW_GetFittedValues.m

--- MATLAB Code/ResultsSet2/YELLOW_LEDs/YELLOW_GetFittedValues.m
+++ MATLAB Code/ResultsSet2/YELLOW_LEDs/YELLOW_GetFittedValues.m
@@ -1,13 +1,13 @@
 % Fitting the spectrum values
 
 % YELLOW1
-spectrum_O1_TL_fit = fit(wavelength_TL, spectrum_O1_TL_norm,'gauss6'); %fitting of ThorLabs normalized spectrum
-spectrum_O1_AS_fit = fit(wavelength_AS, spectrum_O1_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+spectrum_Y1_TL_fit = fit(wavelength_TL, spectrum_Y1_TL_norm,'gauss8'); %fitting of ThorLabs normalized spectrum
+spectrum_Y1_AS_fit = fit(wavelength_AS, spectrum_Y1_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
 
 % YELLOW2
-spectrum_O2_TL_fit = fit(wavelength_TL, spectrum_O2_TL_norm,'gauss5'); %fitting of ThorLabs normalized spectrum
-spectrum_O2_AS_fit = fit(wavelength_AS, spectrum_O2_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+spectrum_Y2_TL_fit = fit(wavelength_TL, spectrum_Y2_TL_norm,'gauss6'); %fitting of ThorLabs normalized spectrum
+spectrum_Y2_AS_fit = fit(wavelength_AS, spectrum_Y2_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
 
 % YELLOW_STRIP
-spectrum_O3_TL_fit = fit(wavelength_TL, spectrum_O3_TL_norm,'gauss6'); %fitting of ThorLabs normalized spectrum
-spectrum_O3_AS_fit = fit(wavelength_AS, spectrum_O3_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+spectrum_YSTRIP_TL_fit = fit(wavelength_TL, spectrum_YSTRIP_TL_norm,'gauss6'); %fitting of ThorLabs normalized spectrum
+spectrum_YSTRIP_AS_fit = fit(wavelength_AS, spectrum_YSTRIP_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
