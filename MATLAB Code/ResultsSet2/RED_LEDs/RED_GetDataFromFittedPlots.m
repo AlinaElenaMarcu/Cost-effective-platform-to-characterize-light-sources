0890784e745b1a83860ded08b7cf29bf51e4ae8c
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 18:52:19 +0300

    update RED_GetDatafromFittedPlots

---
+++ MATLAB Code/ResultsSet2/RED_LEDs/RED_GetDataFromFittedPlots.m
@@ -0,0 +1,72 @@
+% Get xData and yData from fitted plots
+
+% RED1
+spectrum_R1_TL_xData = get(plot(spectrum_R1_TL_fit),'xData')';
+spectrum_R1_TL_yData = get(plot(spectrum_R1_TL_fit),'yData')';
+
+spectrum_R1_AS_xData = get(plot(spectrum_R1_AS_fit),'xData')';
+spectrum_R1_AS_yData = get(plot(spectrum_R1_AS_fit),'yData')';
+
+% RED2
+spectrum_R2_TL_xData = get(plot(spectrum_R2_TL_fit),'xData')';
+spectrum_R2_TL_yData = get(plot(spectrum_R2_TL_fit),'yData')';
+
+spectrum_R2_AS_xData = get(plot(spectrum_R2_AS_fit),'xData')';
+spectrum_R2_AS_yData = get(plot(spectrum_R2_AS_fit),'yData')';
+
+% RED3
+spectrum_R3_TL_xData = get(plot(spectrum_R3_TL_fit),'xData')';
+spectrum_R3_TL_yData = get(plot(spectrum_R3_TL_fit),'yData')';
+
+spectrum_R3_AS_xData = get(plot(spectrum_R3_AS_fit),'xData')';
+spectrum_R3_AS_yData = get(plot(spectrum_R3_AS_fit),'yData')';
+
+% RED4
+spectrum_R4_TL_xData = get(plot(spectrum_R4_TL_fit),'xData')';
+spectrum_R4_TL_yData = get(plot(spectrum_R4_TL_fit),'yData')';
+
+spectrum_R4_AS_xData = get(plot(spectrum_R4_AS_fit),'xData')';
+spectrum_R4_AS_yData = get(plot(spectrum_R4_AS_fit),'yData')';
+
+% RED5
+spectrum_R5_TL_xData = get(plot(spectrum_R5_TL_fit),'xData')';
+spectrum_R5_TL_yData = get(plot(spectrum_R5_TL_fit),'yData')';
+
+spectrum_R5_AS_xData = get(plot(spectrum_R5_AS_fit),'xData')';
+spectrum_R5_AS_yData = get(plot(spectrum_R5_AS_fit),'yData')';
+
+% RED6
+spectrum_R6_TL_xData = get(plot(spectrum_R6_TL_fit),'xData')';
+spectrum_R6_TL_yData = get(plot(spectrum_R6_TL_fit),'yData')';
+
+spectrum_R6_AS_xData = get(plot(spectrum_R6_AS_fit),'xData')';
+spectrum_R6_AS_yData = get(plot(spectrum_R6_AS_fit),'yData')';
+
+% RED8_KING_SRCE
+spectrum_R8_TL_xData = get(plot(spectrum_R8_TL_fit),'xData')';
+spectrum_R8_TL_yData = get(plot(spectrum_R8_TL_fit),'yData')';
+
+spectrum_R8_AS_xData = get(plot(spectrum_R8_AS_fit),'xData')';
+spectrum_R8_AS_yData = get(plot(spectrum_R8_AS_fit),'yData')';
+
+% RED_RG_2THT_RED7
+spectrum_RRG2T_TL_xData = get(plot(spectrum_RRG2T_TL_fit),'xData')';
+spectrum_RRG2T_TL_yData = get(plot(spectrum_RRG2T_TL_fit),'yData')';
+
+spectrum_RRG2T_AS_xData = get(plot(spectrum_RRG2T_AS_fit),'xData')';
+spectrum_RRG2T_AS_yData = get(plot(spectrum_RRG2T_AS_fit),'yData')';
+
+% RED_RG_3THT
+spectrum_RRG3T_TL_xData = get(plot(spectrum_RRG3T_TL_fit),'xData')';
+spectrum_RRG3T_TL_yData = get(plot(spectrum_RRG3T_TL_fit),'yData')';
+
+spectrum_RRG3T_AS_xData = get(plot(spectrum_RRG3T_AS_fit),'xData')';
+spectrum_RRG3T_AS_yData = get(plot(spectrum_RRG3T_AS_fit),'yData')';
+
+% RED_STRIP
+spectrum_RSTRIP_TL_xData = get(plot(spectrum_RSTRIP_TL_fit),'xData')';
+spectrum_RSTRIP_TL_yData = get(plot(spectrum_RSTRIP_TL_fit),'yData')';
+
+spectrum_RSTRIP_AS_xData = get(plot(spectrum_RSTRIP_AS_fit),'xData')';
+spectrum_RSTRIP_AS_yData = get(plot(spectrum_RSTRIP_AS_fit),'yData')';
+
