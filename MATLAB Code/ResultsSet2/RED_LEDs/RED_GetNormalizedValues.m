26e483b7906f1f7e03a47ee62587ef027e30561c
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 18:33:44 +0300

    update RED_GetNormalizedValues

---
+++ MATLAB Code/ResultsSet2/RED_LEDs/RED_GetNormalizedValues.m
@@ -0,0 +1,41 @@
+% Normalize spectrum values
+
+% RED1
+spectrum_R1_TL_norm = normalize(spectrum_R1_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_R1_AS_norm = normalize(spectrum_R1_AS,'norm','inf'); % AS7262 values normalized
+
+% RED2
+spectrum_R2_TL_norm = normalize(spectrum_R2_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_R2_AS_norm = normalize(spectrum_R2_AS,'norm','inf'); % AS7262 values normalized
+
+% RED3
+spectrum_R3_TL_norm = normalize(spectrum_R3_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_R3_AS_norm = normalize(spectrum_R3_AS,'norm','inf'); % AS7262 values normalized
+
+% RED4
+spectrum_R4_TL_norm = normalize(spectrum_R4_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_R4_AS_norm = normalize(spectrum_R4_AS,'norm','inf'); % AS7262 values normalized
+
+% RED5
+spectrum_R5_TL_norm = normalize(spectrum_R5_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_R5_AS_norm = normalize(spectrum_R5_AS,'norm','inf'); % AS7262 values normalized
+
+% RED6
+spectrum_R6_TL_norm = normalize(spectrum_R6_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_R6_AS_norm = normalize(spectrum_R6_AS,'norm','inf'); % AS7262 values normalized
+
+% RED8_KING_SRCE
+spectrum_R8_TL_norm = normalize(spectrum_R8_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_R8_AS_norm = normalize(spectrum_R8_AS,'norm','inf'); % AS7262 values normalized
+
+% RED_RG_2THT_RED7
+spectrum_RRG2T_TL_norm = normalize(spectrum_RRG2T_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_RRG2T_AS_norm = normalize(spectrum_RRG2T_AS,'norm','inf'); % AS7262 values normalized
+
+% RED_RG_3THT
+spectrum_RRG3T_TL_norm = normalize(spectrum_RRG3T_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_RRG3T_AS_norm = normalize(spectrum_RRG3T_AS,'norm','inf'); % AS7262 values normalized
+
+% RED_STRIP
+spectrum_RSTRIP_TL_norm = normalize(spectrum_RSTRIP_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_RSTRIP_AS_norm = normalize(spectrum_RSTRIP_AS,'norm','inf'); % AS7262 values normalized
