9ca390923e6aee1947130d5d9aa98cac1b55d079
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-24 16:35:01 +0300

    update ORANGE_GetNormalizedValues

---
+++ MATLAB Code/ResultsSet2/ORANGE_LEDs/ORANGE_GetNormalizedValues.m
@@ -0,0 +1,26 @@
+% Normalize spectrum values
+
+% ORANGE1
+spectrum_O1_TL_norm = normalize(spectrum_O1_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_O1_AS_norm = normalize(spectrum_O1_AS,'norm','inf'); % AS7262 values normalized
+
+% ORANGE2
+spectrum_O2_TL_norm = normalize(spectrum_O2_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_O2_AS_norm = normalize(spectrum_O2_AS,'norm','inf'); % AS7262 values normalized
+
+% ORANGE3
+spectrum_O3_TL_norm = normalize(spectrum_O3_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_O3_AS_norm = normalize(spectrum_O3_AS,'norm','inf'); % AS7262 values normalized
+
+% ORANGE4
+spectrum_O4_TL_norm = normalize(spectrum_O4_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_O4_AS_norm = normalize(spectrum_O4_AS,'norm','inf'); % AS7262 values normalized
+
+% ORANGE5
+spectrum_O5_TL_norm = normalize(spectrum_O5_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_O5_AS_norm = normalize(spectrum_O5_AS,'norm','inf'); % AS7262 values normalized
+
+% ORANGE6
+spectrum_O6_TL_norm = normalize(spectrum_O6_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_O6_AS_norm = normalize(spectrum_O6_AS,'norm','inf'); % AS7262 values normalized
+
