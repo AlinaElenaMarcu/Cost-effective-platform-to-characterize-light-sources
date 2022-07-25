6fa58ee46741cb5f40bf4283c34c4c307d92487c
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-25 19:30:15 +0300

    Create RED_RS1_Code.m

---
+++ MATLAB Code/ResultsSet1/RED_LEDs/RED_RS1_Code.m
@@ -0,0 +1,3 @@
+% Plot the LEDs in the ORANGE category ThorLabs vs AS7262, Plot data vs
+% fitted values, Peak detect
+% RED1, RED2, RED3, RED4, RED5, RED6, RED8_KING_SRCE, RED_RG_2THT_RED7, RED_RG_3THT, RED_STRIP
