baf3bc3a0f8812f655bfb71a7931d4b837fe2218
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-25 12:56:47 +0300

    Update GREEN_RS1_Code.m

--- MATLAB Code/ResultsSet1/GREEN_LEDs/GREEN_RS1_Code.m
+++ MATLAB Code/ResultsSet1/GREEN_LEDs/GREEN_RS1_Code.m
@@ -1,3 +1,15 @@
 % Plot the LEDs in the GREEN category ThorLabs vs AS7262, Plot data vs
 % fitted values, Peak detect
 % GREEN1, GREEN2, GREEN3, GREEN4, GREEN5_KING_SGC, GREEN_RG_2THT, GREEN_RG_3THT, GREEN_STRIP
+
+% Load GREEN LEDs variables -- ThorLabs and AS7262
+
+GREEN_GetVariablesFromFiles;
+
+% Normalize
+
+GREEN_GetNormalizedValues;
+
+% Fitting
+
+GREEN_GetFittedValues;
