cf899ba90e34e9c07715efbc3e662a071450195c
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-25 12:55:52 +0300

    Create GREEN_RS1_Code.m

---
+++ MATLAB Code/ResultsSet1/GREEN_LEDs/GREEN_RS1_Code.m
@@ -0,0 +1,3 @@
+% Plot the LEDs in the GREEN category ThorLabs vs AS7262, Plot data vs
+% fitted values, Peak detect
+% GREEN1, GREEN2, GREEN3, GREEN4, GREEN5_KING_SGC, GREEN_RG_2THT, GREEN_RG_3THT, GREEN_STRIP
