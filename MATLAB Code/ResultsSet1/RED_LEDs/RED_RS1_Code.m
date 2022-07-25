47ca7acf55d62ed526b3fd70dbf3a6901af5978a
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-25 19:31:38 +0300

    Update RED_RS1_Code.m

--- MATLAB Code/ResultsSet1/RED_LEDs/RED_RS1_Code.m
+++ MATLAB Code/ResultsSet1/RED_LEDs/RED_RS1_Code.m
@@ -1,3 +1,18 @@
-% Plot the LEDs in the ORANGE category ThorLabs vs AS7262, Plot data vs
+% Plot the LEDs in the RED category ThorLabs vs AS7262, Plot data vs
 % fitted values, Peak detect
 % RED1, RED2, RED3, RED4, RED5, RED6, RED8_KING_SRCE, RED_RG_2THT_RED7, RED_RG_3THT, RED_STRIP
+
+% Load RED LEDs variables -- ThorLabs and AS7262
+
+RED_GetVariablesFromFiles;
+
+% Normalize
+
+RED_GetNormalizedValues;
+
+% Fitting
+
+RED_GetFittedValues;
+
+
+
