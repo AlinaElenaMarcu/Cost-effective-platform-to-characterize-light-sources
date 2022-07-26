879835626c7dd1be6177bb516aa5045ee92d1518
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 20:57:08 +0300

    Update YELLOW_RS2_Code.m

--- MATLAB Code/ResultsSet2/YELLOW_LEDs/YELLOW_RS2_Code.m
+++ MATLAB Code/ResultsSet2/YELLOW_LEDs/YELLOW_RS2_Code.m
@@ -25,13 +25,13 @@ plot(spectrum_Y2_TL_xData,spectrum_Y2_TL_yData,'Color',[0.4940, 0.1840, 0.5560])
 hold on;
 plot(spectrum_YSTRIP_TL_xData,spectrum_YSTRIP_TL_yData,'Color',[0, 0.4470, 0.7410]); % #0072BD color
 hold on;
-legend("YELLOW1","YELLOW2","YELLOW STRIP",'Location','northwest');
+legend("YELLOW1","YELLOW2","YELLOW STRIP",'NumColumns',3,'Location','southoutside');
 %axis([300 700 0 inf])
 axis([450 650 0 1])
 xlabel("Wavelength [nm]");
 ylabel("Intensity (normalized)");
 title("YELLOW ThorLabs Emission Spectrum")
-
+saveas(gcf,'YELLOW_ThorLabs_Emission_Spectrum.png')
 
 figure(2)
 plot(spectrum_Y1_AS_xData,spectrum_Y1_AS_yData,'Color',[0.6350, 0.0780, 0.1840]); % #A2142F color
@@ -40,8 +40,9 @@ plot(spectrum_Y2_AS_xData,spectrum_Y2_AS_yData,'Color',[0.4940, 0.1840, 0.5560])
 hold on;
 plot(spectrum_YSTRIP_AS_xData,spectrum_YSTRIP_AS_yData,'Color',[0, 0.4470, 0.7410]); % #0072BD color
 hold on;
-legend("YELLOW1","YELLOW2","YELLOW STRIP",'Location','northwest');
+legend("YELLOW1","YELLOW2","YELLOW STRIP",'NumColumns',3,'Location','southoutside');
 axis([450 650 0 1])
 xlabel("Wavelength [nm]");
 ylabel("Intensity (normalized)");
 title("YELLOW AS7262 Emission Spectrum")
+saveas(gcf,'YELLOW_AS7262_Emission_Spectrum.png')
