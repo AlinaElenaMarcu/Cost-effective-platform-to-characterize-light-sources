21e1495bd5548390a5992b9d46042a7f85419508
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:37:26 +0300

    Update Nexus5X_GetDatafromFittedPlot.m

--- MATLAB Code/ResultsSet3/Nexus5X_GetDatafromFittedPlot.m
+++ MATLAB Code/ResultsSet3/Nexus5X_GetDatafromFittedPlot.m
@@ -4,17 +4,29 @@
 spectrum_G1_AS_xData = get(plot(spectrum_G1_AS_fit),'xData')';
 spectrum_G1_AS_yData = get(plot(spectrum_G1_AS_fit),'yData')';
 
+spectrum_nG1_AS_xData = get(plot(spectrum_nG1_AS_fit),'xData')';
+spectrum_nG1_AS_yData = get(plot(spectrum_nG1_AS_fit),'yData')';
+
 % Nexus5X_Gain3_7
 spectrum_G3_7_AS_xData = get(plot(spectrum_G3_7_AS_fit),'xData')';
 spectrum_G3_7_AS_yData = get(plot(spectrum_G3_7_AS_fit),'yData')';
+
+spectrum_nG3_7_AS_xData = get(plot(spectrum_nG3_7_AS_fit),'xData')';
+spectrum_nG3_7_AS_yData = get(plot(spectrum_nG3_7_AS_fit),'yData')';
 
 % Nexus5X_Gain16
 spectrum_G16_AS_xData = get(plot(spectrum_G16_AS_fit),'xData')';
 spectrum_G16_AS_yData = get(plot(spectrum_G16_AS_fit),'yData')';
 
+spectrum_nG16_AS_xData = get(plot(spectrum_nG16_AS_fit),'xData')';
+spectrum_nG16_AS_yData = get(plot(spectrum_nG16_AS_fit),'yData')';
+
 % Nexus5X_Gain64
 spectrum_G64_AS_xData = get(plot(spectrum_G64_AS_fit),'xData')';
 spectrum_G64_AS_yData = get(plot(spectrum_G64_AS_fit),'yData')';
+
+spectrum_nG64_AS_xData = get(plot(spectrum_nG64_AS_fit),'xData')';
+spectrum_nG64_AS_yData = get(plot(spectrum_nG64_AS_fit),'yData')';
 
 
 
