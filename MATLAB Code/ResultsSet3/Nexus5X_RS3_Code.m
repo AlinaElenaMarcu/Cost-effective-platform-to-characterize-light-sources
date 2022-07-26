11a43c5969a2b680055bdbdc05366b6285d40b7e
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:05:08 +0300

    Update Nexus5X_RS3_Code.m

--- MATLAB Code/ResultsSet3/Nexus5X_RS3_Code.m
+++ MATLAB Code/ResultsSet3/Nexus5X_RS3_Code.m
@@ -1,2 +1,6 @@
 % Plot the Nexus5X Flash LED for all the measured gains (1, 3.7, 16, 64) --
 % 2 Plots noncalibrated and calibrated values
+
+% Load Nexus5x Flash LED variables -- AS7262
+
+Nexus5X_GetVariablesFromFiles;
