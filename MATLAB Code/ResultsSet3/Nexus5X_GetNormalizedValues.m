d02fb8c4420369e495bf926fb1a175f49aef95fe
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:06:08 +0300

    Update Nexus5X_GetNormalizedValues.m

--- MATLAB Code/ResultsSet3/Nexus5X_GetNormalizedValues.m
+++ MATLAB Code/ResultsSet3/Nexus5X_GetNormalizedValues.m
@@ -1,14 +1,14 @@
 % Normalize spectrum values
 
 % Nexus5X_Gain1
-spectrum_W1_AS_norm = normalize(spectrum_W1_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G1_AS_norm = normalize(spectrum_G1_AS,'norm','inf'); % AS7262 values normalized
 
 % Nexus5X_Gain3_7
-spectrum_W2_AS_norm = normalize(spectrum_W2_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G3_7_AS_norm = normalize(spectrum_G3_7_AS,'norm','inf'); % AS7262 values normalized
 
 % Nexus5X_Gain16
-spectrum_W3_AS_norm = normalize(spectrum_W3_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G16_AS_norm = normalize(spectrum_G16_AS,'norm','inf'); % AS7262 values normalized
 
 % Nexus5X_Gain64
-spectrum_WSTRIP_AS_norm = normalize(spectrum_WSTRIP_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G64_AS_norm = normalize(spectrum_G64_AS,'norm','inf'); % AS7262 values normalized
 
