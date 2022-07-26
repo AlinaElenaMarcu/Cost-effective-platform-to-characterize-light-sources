691120d39256842e9c3fd980f87ca0cdd1a2b31d
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:05:05 +0300

    Update Nexus5X_GetNormalizedValues.m

--- MATLAB Code/ResultsSet3/Nexus5X_GetNormalizedValues.m
+++ MATLAB Code/ResultsSet3/Nexus5X_GetNormalizedValues.m
@@ -1,18 +1,14 @@
 % Normalize spectrum values
 
-% WHITE_AS7263
-spectrum_W1_TL_norm = normalize(spectrum_W1_TL,'norm','inf'); % ThorLabs values normalized
+% Nexus5X_Gain1
 spectrum_W1_AS_norm = normalize(spectrum_W1_AS,'norm','inf'); % AS7262 values normalized
 
-% WHITE_LUPA
-spectrum_W2_TL_norm = normalize(spectrum_W2_TL,'norm','inf'); % ThorLabs values normalized
+% Nexus5X_Gain3_7
 spectrum_W2_AS_norm = normalize(spectrum_W2_AS,'norm','inf'); % AS7262 values normalized
 
-% WHITE_NEXUS5X_FLASH
-spectrum_W3_TL_norm = normalize(spectrum_W3_TL,'norm','inf'); % ThorLabs values normalized
+% Nexus5X_Gain16
 spectrum_W3_AS_norm = normalize(spectrum_W3_AS,'norm','inf'); % AS7262 values normalized
 
-% WHITE_STRIP
-spectrum_WSTRIP_TL_norm = normalize(spectrum_WSTRIP_TL,'norm','inf'); % ThorLabs values normalized
+% Nexus5X_Gain64
 spectrum_WSTRIP_AS_norm = normalize(spectrum_WSTRIP_AS,'norm','inf'); % AS7262 values normalized
 
