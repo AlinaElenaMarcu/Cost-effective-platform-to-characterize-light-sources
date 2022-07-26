aad582beca73e1ab7183b9bfad07554e861e9e0d
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 20:43:51 +0300

    Update BLUE_RS2_Code.m

--- MATLAB Code/ResultsSet2/BLUE_LEDs/BLUE_RS2_Code.m
+++ MATLAB Code/ResultsSet2/BLUE_LEDs/BLUE_RS2_Code.m
@@ -42,6 +42,7 @@ axis([450 650 0 1])
 xlabel("Wavelength [nm]");
 ylabel("Intensity (normalized)");
 title("BLUE ThorLabs Emission Spectrum")
+saveas(gcf,'BLUE1_ThorLabs_Emission_Spectrum.png')
 
 
 figure(2)
@@ -66,7 +67,7 @@ axis([450 650 0 1])
 xlabel("Wavelength [nm]");
 ylabel("Intensity (normalized)");
 title("BLUE AS7262 Emission Spectrum")
-
+saveas(gcf,'BLUE1_AS7262_Emission_Spectrum.png')
 
 
 
