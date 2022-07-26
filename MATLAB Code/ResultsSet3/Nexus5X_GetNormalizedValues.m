94e3518f12da688c2a570df04e0337f35cd3d74a
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:22:38 +0300

    Update Nexus5X_GetNormalizedValues.m

--- MATLAB Code/ResultsSet3/Nexus5X_GetNormalizedValues.m
+++ MATLAB Code/ResultsSet3/Nexus5X_GetNormalizedValues.m
@@ -1,14 +1,18 @@
 % Normalize spectrum values
 
 % Nexus5X_Gain1
-spectrum_G1_AS_norm = normalize(spectrum_G1_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G1_AS_norm = normalize(spectrum_G1_AS,'norm','inf'); % calibrated value normalized
+spectrum_nG1_AS_norm = normalize(spectrum_nG1_AS,'norm','inf'); % noncalibrated value normalized
 
 % Nexus5X_Gain3_7
-spectrum_G3_7_AS_norm = normalize(spectrum_G3_7_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G3_7_AS_norm = normalize(spectrum_G3_7_AS,'norm','inf'); % calibrated value normalized
+spectrum_nG3_7_AS_norm = normalize(spectrum_nG3_7_AS,'norm','inf'); % noncalibrated value normalized
 
 % Nexus5X_Gain16
-spectrum_G16_AS_norm = normalize(spectrum_G16_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G16_AS_norm = normalize(spectrum_G16_AS,'norm','inf'); % calibrated value normalized
+spectrum_nG16_AS_norm = normalize(spectrum_nG16_AS,'norm','inf'); % noncalibrated value normalized
 
 % Nexus5X_Gain64
-spectrum_G64_AS_norm = normalize(spectrum_G64_AS,'norm','inf'); % AS7262 values normalized
+spectrum_G64_AS_norm = normalize(spectrum_G64_AS,'norm','inf'); % calibrated value normalized
+spectrum_nG64_AS_norm = normalize(spectrum_nG64_AS,'norm','inf'); % noncalibrated value normalized
 
