376a571149702412ed4b1cd67cb349f7cfbfcdaf
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:14:08 +0300

    Update Nexus5X_RS3_Code.m

--- MATLAB Code/ResultsSet3/Nexus5X_RS3_Code.m
+++ MATLAB Code/ResultsSet3/Nexus5X_RS3_Code.m
@@ -8,3 +8,11 @@ Nexus5X_GetVariablesFromFiles;
 % Normalize
 
 Nexus5X_GetNormalizedValues;
+
+% Fitting
+
+Nexus5X_GetFittedValues;
+
+% Get xData and yData from fitted plots
+
+Nexus5X_GetDatafromFittedPlot;
