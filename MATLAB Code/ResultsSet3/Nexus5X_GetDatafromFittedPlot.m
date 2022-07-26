5e58f6134fc3609d679ef8c408ff3b2e9c99e45a
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:13:42 +0300

    Update Nexus5X_GetDatafromFittedPlot.m

--- MATLAB Code/ResultsSet3/Nexus5X_GetDatafromFittedPlot.m
+++ MATLAB Code/ResultsSet3/Nexus5X_GetDatafromFittedPlot.m
@@ -1,32 +1,20 @@
 % Get xData and yData from fitted plots
 
-% WHITE_AS7263
-spectrum_W1_TL_xData = get(plot(spectrum_W1_TL_fit),'xData')';
-spectrum_W1_TL_yData = get(plot(spectrum_W1_TL_fit),'yData')';
+% Nexus5X_Gain1
+spectrum_G1_AS_xData = get(plot(spectrum_G1_AS_fit),'xData')';
+spectrum_G1_AS_yData = get(plot(spectrum_G1_AS_fit),'yData')';
 
-spectrum_W1_AS_xData = get(plot(spectrum_W1_AS_fit),'xData')';
-spectrum_W1_AS_yData = get(plot(spectrum_W1_AS_fit),'yData')';
+% Nexus5X_Gain3_7
+spectrum_G3_7_AS_xData = get(plot(spectrum_G3_7_AS_fit),'xData')';
+spectrum_G3_7_AS_yData = get(plot(spectrum_G3_7_AS_fit),'yData')';
 
-% WHITE_LUPA
-spectrum_W2_TL_xData = get(plot(spectrum_W2_TL_fit),'xData')';
-spectrum_W2_TL_yData = get(plot(spectrum_W2_TL_fit),'yData')';
+% Nexus5X_Gain16
+spectrum_G16_AS_xData = get(plot(spectrum_G16_AS_fit),'xData')';
+spectrum_G16_AS_yData = get(plot(spectrum_G16_AS_fit),'yData')';
 
-spectrum_W2_AS_xData = get(plot(spectrum_W2_AS_fit),'xData')';
-spectrum_W2_AS_yData = get(plot(spectrum_W2_AS_fit),'yData')';
-
-% WHITE_NEXUS5X_FLASH
-spectrum_W3_TL_xData = get(plot(spectrum_W3_TL_fit),'xData')';
-spectrum_W3_TL_yData = get(plot(spectrum_W3_TL_fit),'yData')';
-
-spectrum_W3_AS_xData = get(plot(spectrum_W3_AS_fit),'xData')';
-spectrum_W3_AS_yData = get(plot(spectrum_W3_AS_fit),'yData')';
-
-% WHITE_STRIP
-spectrum_WSTRIP_TL_xData = get(plot(spectrum_WSTRIP_TL_fit),'xData')';
-spectrum_WSTRIP_TL_yData = get(plot(spectrum_WSTRIP_TL_fit),'yData')';
-
-spectrum_WSTRIP_AS_xData = get(plot(spectrum_WSTRIP_AS_fit),'xData')';
-spectrum_WSTRIP_AS_yData = get(plot(spectrum_WSTRIP_AS_fit),'yData')';
+% Nexus5X_Gain64
+spectrum_G64_AS_xData = get(plot(spectrum_G64_AS_fit),'xData')';
+spectrum_G64_AS_yData = get(plot(spectrum_G64_AS_fit),'yData')';
 
 
 
