e1ce526b5024fa81d5302ac17673ff2473200f39
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-26 13:25:03 +0300

    Update Nexus5X_GetFittedValues.m

--- MATLAB Code/ResultsSet3/Nexus5X_GetFittedValues.m
+++ MATLAB Code/ResultsSet3/Nexus5X_GetFittedValues.m
@@ -1,13 +1,18 @@
 % Fitting the spectrum values
 
 % Nexus5X_Gain1
-spectrum_G1_AS_fit = fit(wavelength_AS, spectrum_G1_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+spectrum_G1_AS_fit = fit(wavelength_AS, spectrum_G1_AS_norm,'pchipinterp'); % fitting of calibrated normalized spectrum
+spectrum_nG1_AS_fit = fit(wavelength_AS, spectrum_nG1_AS_norm,'pchipinterp'); % fitting of noncalibrated normalized spectrum
 
 % Nexus5X_Gain3_7
-spectrum_G3_7_AS_fit = fit(wavelength_AS, spectrum_G3_7_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+spectrum_G3_7_AS_fit = fit(wavelength_AS, spectrum_G3_7_AS_norm,'pchipinterp'); % fitting of calibrated normalized spectrum
+spectrum_nG3_7_AS_fit = fit(wavelength_AS, spectrum_nG3_7_AS_norm,'pchipinterp'); % fitting of noncalibrated normalized spectrum
 
 % Nexus5X_Gain16
-spectrum_G16_AS_fit = fit(wavelength_AS, spectrum_G16_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+spectrum_G16_AS_fit = fit(wavelength_AS, spectrum_G16_AS_norm,'pchipinterp'); % fitting of calibrated normalized spectrum
+spectrum_nG16_AS_fit = fit(wavelength_AS, spectrum_nG16_AS_norm,'pchipinterp'); % fitting of noncalibrated normalized spectrum
 
 % Nexus5X_Gain64
-spectrum_G64_AS_fit = fit(wavelength_AS, spectrum_G64_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+spectrum_G64_AS_fit = fit(wavelength_AS, spectrum_G64_AS_norm,'pchipinterp'); % fitting of calibrated normalized spectrum
+spectrum_nG64_AS_fit = fit(wavelength_AS, spectrum_nG64_AS_norm,'pchipinterp'); % fitting of noncalibrated normalized spectrum
+
