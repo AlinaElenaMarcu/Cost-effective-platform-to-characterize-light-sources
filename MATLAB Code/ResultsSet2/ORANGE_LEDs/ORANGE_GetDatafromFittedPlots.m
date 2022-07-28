edecc972537fb4d2ab5b8b3e66fc0c80a3dfa74b
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-28 13:46:45 +0300

    Update ORANGE_GetDatafromFittedPlots.m

--- MATLAB Code/ResultsSet2/ORANGE_LEDs/ORANGE_GetDatafromFittedPlots.m
+++ MATLAB Code/ResultsSet2/ORANGE_LEDs/ORANGE_GetDatafromFittedPlots.m
@@ -7,12 +7,18 @@ spectrum_O1_TL_yData = get(plot(spectrum_O1_TL_fit),'yData')';
 spectrum_O1_AS_xData = get(plot(spectrum_O1_AS_fit),'xData')';
 spectrum_O1_AS_yData = get(plot(spectrum_O1_AS_fit),'yData')';
 
+spectrum_O1N_AS_xData = get(plot(spectrum_O1N_AS_fit),'xData')';
+spectrum_O1N_AS_yData = get(plot(spectrum_O1N_AS_fit),'yData')';
+
 % ORANGE2
 spectrum_O2_TL_xData = get(plot(spectrum_O2_TL_fit),'xData')';
 spectrum_O2_TL_yData = get(plot(spectrum_O2_TL_fit),'yData')';
 
 spectrum_O2_AS_xData = get(plot(spectrum_O2_AS_fit),'xData')';
 spectrum_O2_AS_yData = get(plot(spectrum_O2_AS_fit),'yData')';
+
+spectrum_O2N_AS_xData = get(plot(spectrum_O2N_AS_fit),'xData')';
+spectrum_O2N_AS_yData = get(plot(spectrum_O2N_AS_fit),'yData')';
 
 % ORANGE3
 spectrum_O3_TL_xData = get(plot(spectrum_O3_TL_fit),'xData')';
@@ -21,12 +27,18 @@ spectrum_O3_TL_yData = get(plot(spectrum_O3_TL_fit),'yData')';
 spectrum_O3_AS_xData = get(plot(spectrum_O3_AS_fit),'xData')';
 spectrum_O3_AS_yData = get(plot(spectrum_O3_AS_fit),'yData')';
 
+spectrum_O3N_AS_xData = get(plot(spectrum_O3N_AS_fit),'xData')';
+spectrum_O3N_AS_yData = get(plot(spectrum_O3N_AS_fit),'yData')';
+
 % ORANGE4
 spectrum_O4_TL_xData = get(plot(spectrum_O4_TL_fit),'xData')';
 spectrum_O4_TL_yData = get(plot(spectrum_O4_TL_fit),'yData')';
 
 spectrum_O4_AS_xData = get(plot(spectrum_O4_AS_fit),'xData')';
 spectrum_O4_AS_yData = get(plot(spectrum_O4_AS_fit),'yData')';
+
+spectrum_O4N_AS_xData = get(plot(spectrum_O4N_AS_fit),'xData')';
+spectrum_O4N_AS_yData = get(plot(spectrum_O4N_AS_fit),'yData')';
 
 % ORANGE5
 spectrum_O5_TL_xData = get(plot(spectrum_O5_TL_fit),'xData')';
