c0821b5ecaeccb3d5f41a6f98f60f2317f8d4ed6
Alina-Elena Marcu <alina_elena.marcu@upb.ro>
2022-07-28 18:34:44 +0300

    update RS4

--- MATLAB Code/ResultsSet4/RS4_Code.m
+++ MATLAB Code/ResultsSet4/RS4_Code.m
@@ -173,3 +173,65 @@ title("Datasheet")
 sgtitle('GREEN Kingbright Emission Spectrum')
 
 saveas(gcf,'GREEN_TvAvD_Emission_Spectrum.png')
+
+%% YELLOW2
+
+% Load YELLOW2 variable and convert to double
+
+% ThorLabs variable
+YELLOW2_TL = load ('D:\Proiecte\GitHub\LEDChar_ATOM2022\Measurements\ThorLabs\YELLOW_LEDs\YELLOW2.mat');
+spectrum_Y2_TL = double(YELLOW2_TL.spectrum);
+
+% AS7262 variable - VIS
+YELLOW2_AS = load ('D:\Proiecte\GitHub\LEDChar_ATOM2022\Measurements\AS7262\LEDs_Gain1\YELLOW_LEDs\YELLOW2.mat');
+spectrum_Y2V_AS = double(YELLOW2_AS.spectrum_C);
+
+% AS7262 variable - NIR
+YELLOW2_NIR_AS = load ('D:\Proiecte\GitHub\LEDChar_ATOM2022\Measurements\AS7262\LEDs_Gain1\YELLOW_LEDs\YELLOW2_NIR.mat');
+spectrum_Y2N_AS = double(YELLOW2_NIR_AS.spectrum_C);
+
+% AS7262 variable - VIS+NIR
+load('wavelengthC_AS.mat')
+spectrum_Y2_AS = [spectrum_Y2V_AS(1:5);spectrum_Y2N_AS(1);spectrum_Y2V_AS(6);spectrum_Y2N_AS(2:6)];
+
+% Normalize spectrum values
+spectrum_Y2_TL_norm = normalize(spectrum_Y2_TL,'norm','inf'); % ThorLabs values normalized
+spectrum_Y2_AS_norm = normalize(spectrum_Y2_AS,'norm','inf'); % AS7262 values normalized
+
+% Fitting the spectrum values
+spectrum_Y2_TL_fit = fit(wavelength_TL, spectrum_Y2_TL_norm,'gauss6'); %fitting of ThorLabs normalized spectrum
+spectrum_Y2_AS_fit = fit(wavelengthC_AS, spectrum_Y2_AS_norm,'pchipinterp'); % fitting of AS7262 normalized spectrum
+
+% Get xData and yData from fitted plots
+spectrum_Y2_TL_xData = get(plot(spectrum_Y2_TL_fit),'xData')';
+spectrum_Y2_TL_yData = get(plot(spectrum_Y2_TL_fit),'yData')';
+
+spectrum_Y2_AS_xData = get(plot(spectrum_Y2_AS_fit),'xData')';
+spectrum_Y2_AS_yData = get(plot(spectrum_Y2_AS_fit),'yData')';
+
+% Plot the emission spectrum ThorLabs vs AS7262 vs Datasheet
+
+spectrum_Y2_DT = imread("YELLOW2_OptoSupply_Spectrum_Datasheet.jpg");
+
+figure(3)
+subplot(2,2,1);
+plot(spectrum_Y2_TL_xData,spectrum_Y2_TL_yData)
+axis([450 730 0 1])
+xlabel("Wavelength [nm]");
+ylabel("Intensity (normalized)");
+title("ThorLabs")
+
+subplot(2,2,2);
+plot(spectrum_Y2_AS_xData,spectrum_Y2_AS_yData)
+axis([450 730 0 1])
+xlabel("Wavelength [nm]");
+ylabel("Intensity (normalized)");
+title("AS7262")
+
+subplot(2,2,[3,4]);
+imshow(spectrum_Y2_DT);
+title("Datasheet")
+
+sgtitle('YELLOW OptoSupply Emission Spectrum')
+
+saveas(gcf,'YELLOW_TvAvD_Emission_Spectrum.png')
